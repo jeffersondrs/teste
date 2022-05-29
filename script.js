2107afe5a239632b86f80dd257d88f9d7299f919
jeffersondrs <jeffersonduarte@id.uff.br>
2022-05-29 18:14:18 -0300

    finished

--- script.js
+++ script.js
@@ -22,7 +22,24 @@ form.on('submit', function (e) {
   $('text-city').text(city.val())
 })
 
-let modalName = $(".text-nome");
-let modalEmail = $(".text-email");
-let modalCity = $(".text-city");
+function cadastro(){
+  var nameUser = document.getElementById('name');
+  var emailUser = document.getElementById('email');
+  var passwordUser = document.getElementById('password');
+  var cityUser = document.getElementById('city');
 
+  var dadosUser = JSON.parse(localStorage.getItem('dadosUser'));
+
+  if(dadosUser == null){
+    dadosUser = [];
+  }
+  var registerUser = {
+    nome: nameUser.value,
+    email: emailUser.value,
+    password: passwordUser.value,
+    city: cityUser.value
+  }
+  dadosUser.push(registerUser);
+
+  localStorage.setItem('dadosUser', JSON.stringify(dadosUser));
+}
